11fe4c22bb665c6b883ab98022d4b4856b1871b8
witalomelo <witalovmelo@gmail.com>
2024-03-19 20:25:20 -0300

    manipulando lista de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -7,12 +7,25 @@ function Favoritos() {
 
   useEffect(() => {
     const minhaLista = localStorage.getItem("@filme"); //pegando a lista de filmes do localStorage
-    setFilmes(JSON.parse(minhaLista) || []); //se minhaLista for null, filmesSalvos será um array vazio
+    setFilmes(JSON.parse(minhaLista) || []); //converte uma string JSON em um objeto JavaScript (array)
   }, []);
+
+  function excluirFilme(id){
+    let filtroFilmes = filmes.filter((item) => { //retornando todos os filmes exceto aquele com id passado como argumento
+      return (item.id !== id)
+    })
+
+    setFilmes(filtroFilmes); //definindo a nova lista tirando o aquele id passado como argumento 
+    localStorage.setItem("@filme", JSON.stringify(filtroFilmes)) //atualizando o repositorio local como a nova lista convertendo a array
+    //em string 
+
+  }
 
   return (
     <div className="meus-filmes">
       <h1>Meus Filmes:</h1>
+      {/*quando não tiver filme salvo */}
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :-( </span>} 
       <ul>
         {filmes.map((item) => {
           return (
@@ -20,7 +33,7 @@ function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           );
