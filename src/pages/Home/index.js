ed4c7bddde23c5b90db854cfa74c39e8620b16f9
witalomelo <witalovmelo@gmail.com>
2024-04-01 11:41:09 -0300

    Criando pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,34 @@
-function Home(){
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+function Home() {
+  const [Filmes, setFilmes] = useState([]);
+
+  
+    useEffect(() => { //useEffect é um hook do React que serve para executar efeitos colaterais em componentes funcionais
+
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "",
+                    language: "pt-BR",
+                    page: 1
+             }
+            })
+
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+
+    }, [])
+
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+//base da url: https://api.themoviedb.org/3/
+
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/"
+});
+
+export default api;
